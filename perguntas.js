60d5fe825b7796ae649ebc563545fc4f735e5091
eduardopio13 <00001110817344sp@al.educacao.sp.gov.br>
2024-11-18 11:28:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    ' O que é um loop em programação?',
+    ' Um loop repete um bloco de código. O for é usado com número fixo de repetições e o while enquanto uma condição for verdadeira.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é a Linha do Equador e qual a sua importância geográfica?',
+    'A Linha do Equador é uma linha imaginária que divide a Terra em duas partes iguais: o Hemisfério Norte e o Hemisfério Sul. Ela está situada a 0° de latitude e é o ponto de referência a partir do qual todas as outras latitudes são medidas.'
+
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'Uma variável armazena dados para serem usados no programa, como números ou textos.'
+)
+
+criaCartao(
+    'lingua inglesa',
+    'Qual é a diferença entre "Present Simple" e "Present Continuous"?',
+    'O Present Simple é usado para falar de hábitos, rotinas ou fatos gerais, como em "I work every day" (Eu trabalho todo dia). O Present Continuous é usado para ações que estão acontecendo no momento da fala, como em "I am working now" (Eu estou trabalhando agora).'
+)
